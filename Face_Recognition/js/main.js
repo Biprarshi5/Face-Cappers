fb86f257a1ada1f4302ff27f5a5e7cfce7147d74
Biprarshi5 <biprarshi1@gmail.com>
2025-08-06 10:12:37 +0530

    Add files via upload

---
+++ Face_Recognition/js/main.js
@@ -0,0 +1,137 @@
+
+const video = document.getElementById("video");
+const isScreenSmall = window.matchMedia("(max-width: 700px)");
+let predictedAges = [];
+
+/****Loading the model ****/
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models")
+]).then(startVideo);
+
+function startVideo() {
+  
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+}
+function getLabeledFaceDescriptions() {
+  const labels = ["Sanjiv", "Messi", "Data"];
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = [];
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+       
+       
+        console.log(`hello ${label}`);
+      }
+     
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+      
+    })
+  );
+}
+
+video.addEventListener("play", async()=>{
+  const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+  
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+
+    const displaySize = {width: video.width, height: video.height};
+faceapi.matchDimensions(canvas, displaySize);
+
+setInterval(async ()=>{
+  const detections = await faceapi
+  .detectAllFaces(video)
+  .withFaceLandmarks()
+  .withFaceDescriptors();
+
+  const resizedDetections = faceapi.resizeResults(detections, displaySize)
+  canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+  const results = resizedDetections.map((d) => {
+    return faceMatcher.findBestMatch(d.descriptor);
+  });
+  results.forEach((result, i) => {
+    const box = resizedDetections[i].detection.box;
+    const drawBox = new faceapi.draw.DrawBox(box, {
+      label: result,
+    });
+    drawBox.draw(canvas);
+   
+  });
+}, 100);
+});
+
+/****Fixing the video with based on size size  ****/
+function screenResize(isScreenSmall) {
+  if (isScreenSmall.matches) {
+    video.style.width = "320px";
+  } else {
+    video.style.width = "500px";
+  }
+}
+
+screenResize(isScreenSmall);
+isScreenSmall.addListener(screenResize);
+
+/****Event Listeiner for the video****/
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  let container = document.querySelector(".container");
+  container.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender();
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    /****Drawing the detection box and landmarkes on canvas****/
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
+    /****Setting values to the DOM****/
+    if (resizedDetections && Object.keys(resizedDetections).length > 0) {
+      const age = resizedDetections.age;
+      const interpolatedAge = interpolateAgePredictions(age);
+      const gender = resizedDetections.gender;
+      const expressions = resizedDetections.expressions;
+      const maxValue = Math.max(...Object.values(expressions));
+      const emotion = Object.keys(expressions).filter(
+        item => expressions[item] === maxValue
+      );
+      document.getElementById("age").innerText = `Age - ${interpolatedAge}`;
+      document.getElementById("gender").innerText = `Gender - ${gender}`;
+      document.getElementById("emotion").innerText = `Expression - ${emotion[0]}`;
+    }
+  }, 10);
+});
+
+function interpolateAgePredictions(age) {
+  predictedAges = [age].concat(predictedAges).slice(0, 30);
+  const avgPredictedAge =
+    predictedAges.reduce((total, a) => total + a) / predictedAges.length;
+  return avgPredictedAge;
+}
